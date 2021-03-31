f8225404764fd011a13ea00942d19949d0d70543
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-03-31 15:34:15 +0200

    corrected imports in core

--- mdeq_lib/core/seg_function.py
+++ mdeq_lib/core/seg_function.py
@@ -13,14 +13,14 @@ import torch.nn as nn
 import torch.distributed as dist
 from torch.nn import functional as F
 
-from utils.utils import AverageMeter
-from utils.utils import get_confusion_matrix
-from utils.utils import adjust_learning_rate
-from utils.utils import get_world_size, get_rank
+from mdeq_lib.utils.utils import AverageMeter
+from mdeq_lib.utils.utils import get_confusion_matrix
+from mdeq_lib.utils.utils import adjust_learning_rate
+from mdeq_lib.utils.utils import get_world_size, get_rank
 
 def reduce_tensor(inp):
     """
-    Reduce the loss from all processes so that 
+    Reduce the loss from all processes so that
     process with rank 0 has the averaged results.
     """
     world_size = get_world_size()
@@ -33,7 +33,7 @@ def reduce_tensor(inp):
 
 def train(config, epoch, num_epoch, epoch_iters, base_lr, num_iters,
          trainloader, optimizer, lr_scheduler, model, writer_dict, device):
-    
+
     # Training
     model.train()
     batch_time = AverageMeter()
@@ -58,7 +58,7 @@ def train(config, epoch, num_epoch, epoch_iters, base_lr, num_iters,
         model.zero_grad()
         loss.backward()
         optimizer.step()
-        
+
         if config.TRAIN.LR_SCHEDULER != 'step':
             lr_scheduler.step()
 
@@ -78,17 +78,17 @@ def train(config, epoch, num_epoch, epoch_iters, base_lr, num_iters,
             print_loss = ave_loss.average() / world_size
             msg = 'Epoch: [{}/{}] Iter:[{}/{}], Time: {:.2f}, ' \
                   'lr: {:.6f}, Loss: {:.6f}' .format(
-                      epoch, num_epoch, i_iter, epoch_iters, 
+                      epoch, num_epoch, i_iter, epoch_iters,
                       batch_time.average(), lr, print_loss)
             logging.info(msg)
-            
+
             writer.add_scalar('train_loss', print_loss, global_steps)
             writer_dict['train_global_steps'] = global_steps + 1
             batch_time = AverageMeter()
-        
+
 
 def validate(config, testloader, model, lr_scheduler, epoch, writer_dict, device):
-    
+
     rank = get_rank()
     world_size = get_world_size()
     model.eval()
@@ -134,9 +134,9 @@ def validate(config, testloader, model, lr_scheduler, epoch, writer_dict, device
         writer.add_scalar('valid_mIoU', mean_IoU, global_steps)
         writer_dict['valid_global_steps'] = global_steps + 1
     return print_loss, mean_IoU, IoU_array
-    
 
-def testval(config, test_dataset, testloader, model, 
+
+def testval(config, test_dataset, testloader, model,
         sv_dir='', sv_pred=False):
     model.eval()
     confusion_matrix = np.zeros(
@@ -146,13 +146,13 @@ def testval(config, test_dataset, testloader, model,
             image, label, _, name = batch
             size = label.size()
             pred = test_dataset.multi_scale_inference(
-                        model, 
-                        image, 
-                        scales=config.TEST.SCALE_LIST, 
+                        model,
+                        image,
+                        scales=config.TEST.SCALE_LIST,
                         flip=config.TEST.FLIP_TEST)
-            
+
             if pred.size()[-2] != size[-2] or pred.size()[-1] != size[-1]:
-                pred = F.interpolate(pred, (size[-2], size[-1]), 
+                pred = F.interpolate(pred, (size[-2], size[-1]),
                                    mode='bilinear', align_corners=True)
 
             confusion_matrix += get_confusion_matrix(
@@ -167,7 +167,7 @@ def testval(config, test_dataset, testloader, model,
                 if not os.path.exists(sv_path):
                     os.mkdir(sv_path)
                 test_dataset.save_pred(pred, sv_path, name)
-            
+
             if index % 100 == 0:
                 logging.info('processing: %d images' % index)
                 pos = confusion_matrix.sum(1)
@@ -187,7 +187,7 @@ def testval(config, test_dataset, testloader, model,
 
     return mean_IoU, IoU_array, pixel_acc, mean_acc
 
-def test(config, test_dataset, testloader, model, 
+def test(config, test_dataset, testloader, model,
         sv_dir='', sv_pred=True):
     model.eval()
     with torch.no_grad():
@@ -195,11 +195,11 @@ def test(config, test_dataset, testloader, model,
             image, size, name = batch
             size = size[0]
             pred = test_dataset.multi_scale_inference(
-                        model, 
-                        image, 
-                        scales=config.TEST.SCALE_LIST, 
+                        model,
+                        image,
+                        scales=config.TEST.SCALE_LIST,
                         flip=config.TEST.FLIP_TEST)
-            
+
             if pred.size()[-2] != size[0] or pred.size()[-1] != size[1]:
                 pred = F.interpolate(pred, (size[-2], size[-1]), mode='bilinear', align_corners=True)
 
