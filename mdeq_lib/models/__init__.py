7142919bf1c18aed5ba05febc8b50cb4d3f01eb4
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-03-31 17:21:38 +0200

    corrected model init imports

--- mdeq_lib/models/__init__.py
+++ mdeq_lib/models/__init__.py
@@ -2,5 +2,5 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import models.mdeq
-import models.mdeq_core
+import mdeq_lib.models.mdeq
+import mdeq_lib.models.mdeq_core
