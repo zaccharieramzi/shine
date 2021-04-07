69dd021645c1a6b0911b0813d1516e83ae57ca06
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-04-07 09:44:34 +0200

    added evaluation script for classifier

---
+++ mdeq_lib/evaluate/cls_valid.py
@@ -0,0 +1,101 @@
+# Modified based on the HRNet repo.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import os
+import sys
+import shutil
+import pprint
+
+import torch
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+import torch.utils.data.distributed
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+import mdeq_lib.models as models
+from mdeq_lib.config import config
+from mdeq_lib.core.cls_function import validate
+from mdeq_lib.training.cls_train import update_config_w_args
+from mdeq_lib.utils.modelsummary import get_model_summary
+from mdeq_lib.utils.utils import create_logger
+
+
+def evaluate_classifier(n_gpus=1, dataset='imagenet', model_size='SMALL', shine=False):
+    args = update_config_w_args(
+        n_gpus=n_gpus,
+        dataset=dataset,
+        model_size=model_size,
+    )
+
+    logger, final_output_dir, tb_log_dir = create_logger(
+        config, args.cfg, 'valid', shine=shine)
+
+    logger.info(pprint.pformat(args))
+    logger.info(pprint.pformat(config))
+
+    # cudnn related setting
+    cudnn.benchmark = config.CUDNN.BENCHMARK
+    torch.backends.cudnn.deterministic = config.CUDNN.DETERMINISTIC
+    torch.backends.cudnn.enabled = config.CUDNN.ENABLED
+
+    model = eval('models.'+config.MODEL.NAME+'.get_cls_net')(
+        config, shine=shine)
+
+    dump_input = torch.rand(
+        (1, 3, config.MODEL.IMAGE_SIZE[1], config.MODEL.IMAGE_SIZE[0])
+    )
+    logger.info(get_model_summary(model, dump_input))
+
+    if config.TEST.MODEL_FILE:
+        logger.info('=> loading model from {}'.format(config.TEST.MODEL_FILE))
+        model.load_state_dict(torch.load(config.TEST.MODEL_FILE))
+    else:
+        model_state_file = os.path.join(final_output_dir,
+                                        'final_state.pth.tar')
+        logger.info('=> loading model from {}'.format(model_state_file))
+        model.load_state_dict(torch.load(model_state_file))
+
+    gpus = list(config.GPUS)
+    model = torch.nn.DataParallel(model, device_ids=gpus).cuda()
+
+    # define loss function (criterion) and optimizer
+    criterion = torch.nn.CrossEntropyLoss().cuda()
+
+    # Data loading code
+    valdir = os.path.join(config.DATASET.ROOT,
+                          config.DATASET.TEST_SET)
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+
+    valid_loader = torch.utils.data.DataLoader(
+        datasets.ImageFolder(valdir, transforms.Compose([
+            transforms.Resize(int(config.MODEL.IMAGE_SIZE[0] / 0.875)),
+            transforms.CenterCrop(config.MODEL.IMAGE_SIZE[0]),
+            transforms.ToTensor(),
+            normalize,
+        ])),
+        batch_size=config.TEST.BATCH_SIZE_PER_GPU*len(gpus),
+        shuffle=False,
+        num_workers=config.WORKERS,
+        pin_memory=True
+    )
+
+    # evaluate on validation set
+    validate(
+        config,
+        valid_loader,
+        model,
+        criterion,
+        None,
+        None,
+        final_output_dir,
+        tb_log_dir,
+        None,
+    )
