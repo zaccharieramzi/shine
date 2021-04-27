3413f233f084f84d31ad68030f43fd96a1bd241e
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-04-27 14:00:31 +0200

    corrected loading in cls grad

--- mdeq_lib/debug/cls_grad.py
+++ mdeq_lib/debug/cls_grad.py
@@ -180,7 +180,7 @@ def train_classifier(
             checkpoint = torch.load(model_state_file)
             last_epoch = checkpoint['epoch']
             best_perf = checkpoint['perf']
-            model.module.load_state_dict(checkpoint['state_dict'])
+            model.load_state_dict(checkpoint['state_dict'])
 
             # Update weight decay if needed
             checkpoint['optimizer']['param_groups'][0]['weight_decay'] = config.TRAIN.WD
