1a85a1f5b3aa5284d16c0d0aac26dea14de6f73f
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-05-03 15:49:34 +0200

    made sure writing of fallback prop happens

--- mdeq_lib/modules/deq2d.py
+++ mdeq_lib/modules/deq2d.py
@@ -157,6 +157,8 @@ class DEQModule2d(nn.Module):
                 # value
                 fallback_mask = dl_df_est.view(bsz, -1).norm(dim=1) > 1.3 * grad.view(bsz, -1).norm(dim=1)
                 dl_df_est = fallback_mask * grad + ~fallback_mask * dl_df_est
+                if dl_df_est.get_device() == 0:
+                    writer.add_scalar('backward/fallback_prop', torch.sum(fallback_mask) / bsz, train_step)
             elif fpn:
                 dl_df_est = grad
             if not(shine or fpn) or gradient_correl or gradient_ratio:
@@ -229,8 +231,6 @@ class DEQModule2d(nn.Module):
                             writer.add_scalar('backward/nstep', result_info['nstep'], train_step)
                             writer.add_scalar('backward/lowest_step', result_info['lowest_step'], train_step)
                             writer.add_scalar('backward/final_trace', result_info['new_trace'][lowest_step], train_step)
-                        if shine:
-                            writer.add_scalar('backward/fallback_prop', torch.sum(fallback_mask) / bsz, train_step)
 
 
 
