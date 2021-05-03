c92c7f65d21ff142dde27d91a0440eee32393c0e
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-05-03 14:48:37 +0200

    made fallback sample wise (instead of batch wise) and made sure to log fallback proportion

--- mdeq_lib/modules/deq2d.py
+++ mdeq_lib/modules/deq2d.py
@@ -149,16 +149,14 @@ class DEQModule2d(nn.Module):
             args = ctx.args
             threshold, train_step, writer, qN_tensors, shine, fpn, gradient_correl, gradient_ratio = args[-8:]
             Us, VTs, nstep = qN_tensors
-            fallback = False
             if shine:
                 dl_df_est = - rmatvec(Us[:,:,:,:nstep-1], VTs[:,:nstep-1], grad)
                 # This implements a fallback in case our inverse approximation
                 # is completely off.
                 # This hardcoded value should be changed at some point to a config
                 # value
-                if torch.norm(dl_df_est) > 1.3 * torch.norm(grad):
-                    dl_df_est = grad
-                    fallback = True
+                fallback_mask = dl_df_est.view(bsz, -1).norm(dim=1) > 1.3 * grad.view(bsz, -1).norm(dim=1)
+                dl_df_est = fallback_mask * grad + ~fallback_mask * dl_df_est
             elif fpn:
                 dl_df_est = grad
             if not(shine or fpn) or gradient_correl or gradient_ratio:
@@ -235,7 +233,7 @@ class DEQModule2d(nn.Module):
                             writer.add_scalar('backward/lowest_step', result_info['lowest_step'], train_step)
                             writer.add_scalar('backward/final_trace', result_info['new_trace'][lowest_step], train_step)
                         if shine:
-                            writer.add_scalar('backward/fallback', int(fallback), train_step)
+                            writer.add_scalar('backward/fallback_prop', torch.sum(fallback_mask) / bsz, train_step)
 
 
 
