5e0f67f612894ce5447743d8702fdaa478af4268
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-05-19 12:27:09 +0200

    added a simple call test for adj broyden and opa

--- mdeq_lib/tests/modules/adj_broyden_test.py
+++ mdeq_lib/tests/modules/adj_broyden_test.py
@@ -1,3 +1,81 @@
-import torch
+import os
 
+import pytest
+import torch
+import torchvision.transforms as transforms
+
+from mdeq_lib.config import config
+from mdeq_lib.config import update_config
 from mdeq_lib.modules.adj_broyden import adj_broyden
+from mdeq_lib.training.cls_train import update_config_w_args
+
+def setup_model(opa=False):
+    seed = 42
+    restart_from = 50
+    n_epochs = 100
+    pretrained = False
+    n_gpus = 1
+    dataset = 'imagenet'
+    model_size = 'SMALL'
+    use_group_norm = False
+    shine = False
+    fpn = False
+    adjoint_broyden = True
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    args = update_config_w_args(
+        n_epochs=n_epochs,
+        pretrained=pretrained,
+        n_gpus=n_gpus,
+        dataset=dataset,
+        model_size=model_size,
+        use_group_norm=use_group_norm,
+    )
+    print(colored("Setting default tensor type to cuda.FloatTensor", "cyan"))
+    torch.multiprocessing.set_start_method('spawn')
+    torch.set_default_tensor_type('torch.cuda.FloatTensor')
+
+    logger, final_output_dir, tb_log_dir = create_logger(
+        config,
+        args.cfg,
+        'train',
+        shine=shine,
+        fpn=fpn,
+        seed=seed,
+        use_group_norm=use_group_norm,
+        adjoint_broyden=adjoint_broyden,
+        opa=opa,
+    )
+
+    model = eval('models.'+config.MODEL.NAME+'.get_cls_net')(
+        config,
+        shine=shine,
+        fpn=fpn,
+        gradient_correl=False,
+        gradient_ratio=False,
+        adjoint_broyden=adjoint_broyden,
+        opa=opa,
+    ).cuda()
+
+
+    resume_file = f'checkpoint_{restart_from}.pth.tar'
+    model_state_file = os.path.join(final_output_dir, resume_file)
+    checkpoint = torch.load(model_state_file)
+    model.load_state_dict(checkpoint['state_dict'])
+
+    return model
+
+@pytest.mark.parametrize('opa', [True, False])
+def test_adj_broyden(opa):
+    model = setup_model(opa)
+    traindir = os.path.join(config.DATASET.ROOT+'/images', config.DATASET.TRAIN_SET)
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    transform_train = transforms.Compose([
+        transforms.RandomResizedCrop(config.MODEL.IMAGE_SIZE[0]),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        normalize,
+    ])
+    train_dataset = datasets.ImageFolder(traindir, transform_train)
+    model(train_dataset[0].cuda())
