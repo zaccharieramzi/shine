4ced87a2cc904a98f0da3491b1529a71f6032617
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-05-20 14:59:48 +0200

    made validation script uses non blocking

--- mdeq_lib/core/cls_function.py
+++ mdeq_lib/core/cls_function.py
@@ -110,7 +110,7 @@ def validate(config, val_loader, model, criterion, lr_scheduler, epoch, output_d
         end = time.time()
         for i, (input, target) in enumerate(val_loader):
             # compute output
-            output = model(input,
+            output = model(input.cuda(non_blocking=True),
                            train_step=-1)
             target = target.cuda(non_blocking=True)
 
