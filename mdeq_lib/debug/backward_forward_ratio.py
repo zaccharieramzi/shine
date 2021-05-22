33804e5112c4febb87c8469f7b0073c7b3357f11
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-05-22 16:16:03 +0200

    guarded against context

--- mdeq_lib/debug/backward_forward_ratio.py
+++ mdeq_lib/debug/backward_forward_ratio.py
@@ -125,7 +125,11 @@ def eval_ratio_fb_classifier(
         use_group_norm=use_group_norm,
     )
     print(colored("Setting default tensor type to cuda.FloatTensor", "cyan"))
-    torch.multiprocessing.set_start_method('spawn')
+    try:
+        torch.multiprocessing.set_start_method('spawn')
+    except RuntimeError:
+        pass
+
     torch.set_default_tensor_type('torch.cuda.FloatTensor')
 
     logger, final_output_dir, tb_log_dir = create_logger(
