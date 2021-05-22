83311248180f695b26a7bc0ca3954420bac32556
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-05-22 16:11:22 +0200

    corrected params in debug fb ratio script

--- mdeq_lib/debug/backward_forward_ratio.py
+++ mdeq_lib/debug/backward_forward_ratio.py
@@ -257,11 +257,11 @@ def eval_ratio_fb_classifier(
 
 if __name__ == '__main__':
     base_params = dict(
-        n_gpus=n_gpus,
+        n_gpus=1,
         n_epochs=100,
         seed=42,
         restart_from=50,
-        n_samples=200,
+        n_samples=10,
     )
     eval_ratio_fb_classifier(
         dataset='cifar', model_size='LARGE', **base_params
