de3baa891af63a427f8917e8ffb6d7faf75061a0
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-05-23 19:41:51 +0200

    got rid of superfluous copy

--- mdeq_lib/modules/adj_broyden.py
+++ mdeq_lib/modules/adj_broyden.py
@@ -90,7 +90,7 @@ def adj_broyden(
             VTs[:,(n_updates-1) % LBFGS_thres] = vT
             Us[:,:,:,(n_updates-1) % LBFGS_thres] = u
         x_est, delta_x, ite = line_search(update, x_est, gx, g, nstep=nstep, on=ls, compute_g=False)
-        x_temp = x_est.clone().detach().requires_grad_()
+        x_temp = x_est.requires_grad_()
         with torch.enable_grad():
             gx = g(x_temp)
         nstep += 1
@@ -119,7 +119,7 @@ def adj_broyden(
         if adj_type != 'C':
             raise NotImplementedError('Use adj_type C for now')
         else:
-            sigma = gx.clone().detach()
+            sigma = gx
         n_updates += 1
         part_Us, part_VTs = Us[..., :n_updates - 1], VTs[:, :n_updates - 1]
         # a = An^{-1} sigma
