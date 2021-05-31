2fa4b1451381124c4e4f402d850ea65277f316b6
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-05-31 14:45:08 +0200

    made sure cifar training res are saved in a csv

--- paper_trainings_cifar.py
+++ paper_trainings_cifar.py
@@ -1,6 +1,5 @@
 import argparse
 
-import numpy as np
 import pandas as pd
 
 from mdeq_lib.evaluate.cls_valid import evaluate_classifier
@@ -46,31 +45,18 @@ if __name__ == '__main__':
                 dict(fpn=True, refine=True, **base_params),
             ]
 
-    res_all = []
+    res_data = []
     for params in parameters:
         train_classifier(**params)
         eval_params = dict(**params)
         eval_params.pop('n_epochs')
         metrics_names, eval_res = evaluate_classifier(**eval_params)
-        res_all.append(eval_res)
+        res_data.append(
+            {
+                'top1': eval_res,
+                **params
+            }
+        )
 
-    # XXX: save the results in a csv
-    for n_refine in n_refines:
-        perf_orig = [
-            res for (res, params) in zip(res_all, parameters)
-            if not params.get('shine', False) and not params.get('fpn', False) and params['n_refine'] == n_refine
-        ]
-        perf_shine = [
-            res for (res, params) in zip(res_all, parameters)
-            if params.get('shine', False) and params['n_refine'] == n_refine
-        ]
-        perf_fpn = [
-            res for (res, params) in zip(res_all, parameters)
-            if params.get('fpn', False) and params['n_refine'] == n_refine
-        ]
-
-
-        print(f'Descriptive stats for {n_refine}')
-        print('Perf orig', np.mean(perf_orig), np.std(perf_orig))
-        print('Perf shine', np.mean(perf_shine), np.std(perf_shine))
-        print('Perf fpn', np.mean(perf_fpn), np.std(perf_fpn))
+    df_res = pd.DataFrame(res_data)
+    df_res.to_csv('cifar_mdeq_results.csv')
