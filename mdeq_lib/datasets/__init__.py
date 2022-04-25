96014af0c8e643ea5fda4f2c43fde25660d9e095
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-04-25 18:48:25 +0200

    commenting out unused datasets

--- mdeq_lib/datasets/__init__.py
+++ mdeq_lib/datasets/__init__.py
@@ -8,6 +8,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from .cityscapes import Cityscapes as cityscapes
-from .lip import LIP as lip
-from .pascal_ctx import PASCALContext as pascal_ctx
+# from .cityscapes import Cityscapes as cityscapes
+# from .lip import LIP as lip
+# from .pascal_ctx import PASCALContext as pascal_ctx
